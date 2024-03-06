ab7ee1cba669c364c4e3573f6e298191591a4aad
Laban-Tran <112023331+Laban-Tran@users.noreply.github.com>
2024-03-05 16:08:49 -0800

    Update addUpVector.cu

--- addUpVector.cu
+++ addUpVector.cu
@@ -4,17 +4,22 @@
 #include <cuda_runtime.h>
 
 __global__ void addUpVector(int* input, int N) {
-	int threadIndex = threadIdx.x;
-	for (int gait=1; gait<N; gait*=2) {
-		if(threadIndex % (2*gait) == 0 && gait < N)
+	int threadIndex = threadIdx.x + threadIdx.x;
+
+	for (int gait=1; gait<N; gait+=gait) {
+		if(threadIndex % (2*gait) == 0)
 			input[threadIndex]+= input[threadIndex + gait];
-		__syncthreads();
+		else
+			break;
 	}
+	__syncthreads();
+
 }
 
 int main(){
 
-	int length = 1000;
+	int threads = 501;
+	int length = threads * 2;
 
 	int host_v[length];
 	for (int i = 0; i < length; i++) {
@@ -25,17 +30,18 @@ int main(){
 	cudaMalloc((void**)&device_v, length*sizeof(int));
 	cudaMemcpy(device_v, host_v, length*sizeof(int),cudaMemcpyHostToDevice);
 
-	dim3 blockSize(length);
+	dim3 blockSize(threads);
 	dim3 gridSize(1);
 
 	addUpVector<<<gridSize,blockSize>>>(device_v, length);
 
 	cudaMemcpy(host_v, device_v, length*sizeof(int),cudaMemcpyDeviceToHost);
 
-	printf("%d \n\n\n", host_v[0]);
+	printf("%d", host_v[0]);
 
 	cudaFree(device_v);
 
 	return 0;
 
 }
+
